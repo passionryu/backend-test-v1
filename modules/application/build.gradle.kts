43867b490dcbe2a9cc6fb2b97287f2bf75b32b12
passionryu <rsy1225@naver.com>
2025-10-17 03:08:53 +0900

    ➖ [의존성 제거] : application -> external 직접 의존하는 의존성 제거 (핵사고날 아키텍처의 경계 위반)

--- modules/application/build.gradle.kts
+++ modules/application/build.gradle.kts
@@ -11,10 +11,9 @@ dependencies {
     // Only need Spring annotations (@Service) for this module
     implementation("org.springframework:spring-context")
 
-    // For Using TestPgClient
-    implementation(project(":modules:external:pg-client"))
-
     // For using common dir
     implementation(project(":modules:common"))
 
+    // For Using TestPgClient (핵사고날 아키텍처의 경계 위반으로 인한 주석 처리)
+    // implementation(project(":modules:external:pg-client"))
 }
