2df33f1a9973657043dec63fc01dddabb615d0cb
passionryu <rsy1225@naver.com>
2025-11-02 00:20:47 +0900

    🐛 [버그 수정] : 카드 중간 값 마스킹

--- modules/external/pg-client/src/main/kotlin/im/bigs/pg/external/pg/helper/buildEncryptedRequestBody.kt
+++ modules/external/pg-client/src/main/kotlin/im/bigs/pg/external/pg/helper/buildEncryptedRequestBody.kt
@@ -6,7 +6,7 @@ import im.bigs.pg.common.crypto.AesGcmEncryptor
 public fun buildEncryptedRequestBody(request: PgApproveRequest, apiKey:String , ivBase64: String): Map<String, String> {
     val plainJson = """
         {
-            "cardNumber": "${request.cardBin}-${request.cardBin}-${request.cardBin}-${request.cardLast4}",
+            "cardNumber": "${request.cardBin}-****-****-${request.cardLast4}",
             "birthDate": "${request.birthDate}",
             "expiry": "${request.expiry}",
             "password": "${request.password}",
